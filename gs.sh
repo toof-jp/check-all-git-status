d7553792eb8dd3d160b58164bdb486e1b57306ce
toof <toof@toof.jp>
2023-01-01 03:20:30 +0900

    update

--- gs.sh
+++ gs.sh
@@ -4,6 +4,9 @@ NC='\033[0m'
 for path in `ghq list -p`
 do
   cd $path
-  echo -e "${GREEN}$path${NC}"
-  git status -s
+  output=$(git status -s)
+  if [ -n "$output" ]; then
+    echo -e "${GREEN}$path${NC}"
+    echo "$output"
+  fi
 done
